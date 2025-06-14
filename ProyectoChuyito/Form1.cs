0b5ab4f757dac412065992cba6a5a0dc8471c746
22030857-Jose-Rosario-Gamez-Valenzuela <22030857@uadeo.mx>
2025-06-14 08:52:30 -0700

    Actualizar Form1.cs

--- ProyectoChuyito/Form1.cs
+++ ProyectoChuyito/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,6 +18,7 @@ namespace ProyectoChuyito
         {
             InitializeComponent();
             this.KeyPreview = true;
+            //FLIPO EDITADO DESDE EL TEÑEFONO
         }
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
